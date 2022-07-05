85db3fb7d3e6f8706865572ad20d405e9ede6e61
Hauskamel <lukas.krojer@web.de>
2022-07-05 21:45:20 +0200

    mail .html and .php added

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,14 +1,23 @@
 <?php
 //get data from form
 
+$gender = $_POST['gender'];
 $name = $_POST['name'];
-$email= $_POST['email'];
+$sirname = $_POST['sirname'];
+$birthday = $_POST['birthday'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
 $message= $_POST['message'];
-$to = "youremail@mail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
+
+$to = "lukas.krojer@web.de";
+
+$subject = "Anmeldeformular";
+
+$txt = "Gender = ". $gender . "Vorname = ". $name . "Nachname = ". $sirname . "Geburtstag = ". $birthday . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: gastrosophen@no-reply.com";
+
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
